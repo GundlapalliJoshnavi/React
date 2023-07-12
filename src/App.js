728ca04a1b60d66a3fb84418b1cfe5dbbdc7a5e8
godavarthi saikrishna <117332446+godavarthisaikrishna@users.noreply.github.com>
2023-07-12 08:31:44 +0530

    compontants created login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import  'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Login from './login';
+import Website from './Website';
+
 {/*LOgin Form */}
 function App() {
   return (<>
-   <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
-    <div className='login'>
-    <form>
-     <h2 className='mb-3'>Login Form</h2>
-     <div className='form-group mb-2'>
-      <label htmlFor='Email' className='form-label'>EmailAddress</label>
-      <input type='email' className='form-control'></input>
-     </div>
-     <div className='form-group mb-2'>
-      <label htmlFor='Password' className='form-label'>Password</label>
-      <input type='password'className='form-control'></input>
-     </div>
-     <button className='btn btn-success block w-100 mt-2' type='submit'>Sign In</button>
-     </form>
-    </div>
-   </div>
+    <Login/>
    </>  
   );
 }
--- src/Website.js
+++ src/Website.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Website.css';
 import track from './track.png';
 import acc from './acc.png';
